e0c38a0aa15a28eb8d60d8c0b48cb1ff17279b17
Gabrial <gabrieldabush@gmail.com>
2025-04-28 12:55:29 +0300

    Save local changes

--- React - ManagingStore/Store/src/firebase.js
+++ React - ManagingStore/Store/src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAaNutOX_DcVBUUEvHzA0fr8HvWVEYCRi0",
-  authDomain: "managestore-3be04.firebaseapp.com",
-  projectId: "managestore-3be04",
-  storageBucket: "managestore-3be04.firebasestorage.app",
-  messagingSenderId: "832989011474",
-  appId: "1:832989011474:web:7cb3f45db8fad47d9ac2cb",
-  measurementId: "G-YQV8HR41E0",
+  apiKey: "*",
+  authDomain: "*",
+  projectId: "*",
+  storageBucket: "*",
+  messagingSenderId: "*",
+  appId: "*",
+  measurementId: "*",
 };
 
 const app = initializeApp(firebaseConfig);
